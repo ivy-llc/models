81397e0a483e83b221a7d25cd5e5b6718e0b52e3
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2023-05-17 15:55:08 +0000

    add some weights matchinig helpers

--- ivy_models/helpers/match_weights_helpers.py
+++ ivy_models/helpers/match_weights_helpers.py
@@ -2,60 +2,62 @@
 import ivy
 
 
-def permute_4d_weights(dictionary: ivy.Container, permute_fn):
+def _permute_4d_weights(dictionary: ivy.Container, permute_fn) -> None:
     """
-    Recursively permutes 4D weights in the dictionary using the provided permutation function.
+    Recursively permutes 4D weights in the dictionary
+                using the provided permutation function.
 
-    Args:
+    Args::
         dictionary (ivy.Container): The dictionary to process.
         permute_fn: The permutation function to apply to the weights.
 
-    Returns:
+    Returns::
         None
     """
     for key, value in list(dictionary.items()):
         if isinstance(value, dict):
-            permute_4d_weights(value, permute_fn)
+            _permute_4d_weights(value, permute_fn)
         else:
             if key.startswith("weight") and len(value.shape) == 4:
                 new_value = {"weight": permute_fn(dictionary.pop(key))}
                 dictionary[key] = ivy.Container(new_value)
 
 
-def rename_w_b(dictionary: ivy.Container):
+def _rename_w_b(dictionary: ivy.Container) -> None:
     """
     Recursively renames the 'weight' and 'bias' keys in the dictionary.
 
-    Args:
+    Args::
         dictionary (ivy.Container): The dictionary to process.
 
-    Returns:
+    Returns::
         None
     """
     keys = list(dictionary.keys())
     for key in keys:
         value = dictionary[key]
         if isinstance(value, dict):
-            rename_w_b(value)
+            _rename_w_b(value)
         if key.startswith("weight"):
             dictionary["w" + key[6:]] = dictionary.pop(key)
         elif key.startswith("bias"):
             dictionary["b" + key[4:]] = dictionary.pop(key)
 
 
-def custom_resnet_renaming(dictionary: ivy.Container):
+def _custom_resnet_renaming(dictionary: ivy.Container) -> None:
     """
-    Recursively renames keys in the dictionary according to custom ResNet naming conventions.
+    Recursively renames keys in the dictionary
+    according to custom ResNet naming conventions.
 
-    Args:
+    Args::
         dictionary (ivy.Container): The dictionary to process.
 
-    Returns:
+    Returns::
         None
     """
     for key, value in list(dictionary.items()):
         if isinstance(value, dict):
-            custom_resnet_renaming(value)
+            _custom_resnet_renaming(value)
 
         if key in ["0", "1"]:
             new_key = "v" + key
