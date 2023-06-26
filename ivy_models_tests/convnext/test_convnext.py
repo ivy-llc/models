c555ed299b02d738fa1f603c64b73ec66ecae103
Tom Edwards <49121269+tomatillos@users.noreply.github.com>
2023-06-26 16:53:37 +0000

    refactored convnext

--- ivy_models_tests/convnext/test_convnext.py
+++ ivy_models_tests/convnext/test_convnext.py
@@ -2,7 +2,7 @@ import os
 import ivy
 import pytest
 import numpy as np
-
+from ivy_models_tests import helpers
 from ivy_models.convnext import convnext
 
 
@@ -11,11 +11,14 @@ from ivy_models.convnext import convnext
 def test_convnext_tiny_img_classification(device, f, fw, batch_shape, load_weights):
     """Test ConvNeXt tiny image classification."""
     num_classes = 1000
-    # load image
     this_dir = os.path.dirname(os.path.realpath(__file__))
-    img = ivy.asarray(
-        np.load(os.path.join(this_dir, "image_convnext.npy")),
+    
+    # Load image
+    img = helpers.load_and_preprocess_img(
+        os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
     )
+    img = ivy.permute_dims(img, (0,3,1,2))
+    # Create model
     model = convnext("tiny", pretrained=load_weights)
 
     logits = model(img)
@@ -26,11 +29,11 @@ def test_convnext_tiny_img_classification(device, f, fw, batch_shape, load_weigh
     # Value test
     if load_weights:
         np_out = ivy.to_numpy(logits[0])
-        true_indices = np.array([0, 1, 78, 303, 111])
+        true_indices = np.array([282, 281, 753, 285, 643])
         calc_indices = np.argsort(np_out)[-5:][::-1]
         assert np.array_equal(true_indices, calc_indices)
 
-        true_logits = np.array([4.9245, 3.5395, 3.2072, 2.9629, 2.9589])
+        true_logits = np.array([8.396295 , 7.442236 , 5.5498557, 5.164446 , 3.710167])
         calc_logits = np.take(np_out, calc_indices)
         assert np.allclose(true_logits, calc_logits, rtol=1e-3)
 
@@ -40,12 +43,15 @@ def test_convnext_tiny_img_classification(device, f, fw, batch_shape, load_weigh
 def test_convnext_small_img_classification(device, f, fw, batch_shape, load_weights):
     """Test ConvNeXt small image classification."""
     num_classes = 1000
-
-    # load image
     this_dir = os.path.dirname(os.path.realpath(__file__))
-    img = ivy.asarray(
-        np.load(os.path.join(this_dir, "image_convnext.npy")),
+    
+    # Load image
+    img = helpers.load_and_preprocess_img(
+        os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
     )
+    img = ivy.permute_dims(img, (0,3,1,2))
+    
+    # Create model
     model = convnext("small", pretrained=load_weights)
     logits = model(img)
 
@@ -55,11 +61,11 @@ def test_convnext_small_img_classification(device, f, fw, batch_shape, load_weig
     if load_weights:
         # Value test
         np_out = ivy.to_numpy(logits[0])
-        true_indices = np.array([623, 111, 21, 1, 644])
+        true_indices = np.array([282, 281, 716, 753, 743])
         calc_indices = np.argsort(np_out)[-5:][::-1]
         assert np.array_equal(true_indices, calc_indices)
 
-        true_logits = np.array([2.5510, 2.5314, 2.4917, 2.2801, 2.2450])
+        true_logits = np.array([9.195629 , 6.0763097, 4.8471575, 3.8018446, 3.520919])
         calc_logits = np.take(np_out, calc_indices)
         assert np.allclose(true_logits, calc_logits, rtol=1e-3)
 
@@ -69,12 +75,15 @@ def test_convnext_small_img_classification(device, f, fw, batch_shape, load_weig
 def test_convnext_base_img_classification(device, f, fw, batch_shape, load_weights):
     """Test ConvNeXt base image classification."""
     num_classes = 1000
-    # load image
     this_dir = os.path.dirname(os.path.realpath(__file__))
-    img = ivy.asarray(
-        np.load(os.path.join(this_dir, "image_convnext.npy")),
+    
+    # Load image
+    img = helpers.load_and_preprocess_img(
+        os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
     )
-
+    img = ivy.permute_dims(img, (0,3,1,2))
+    
+    # Create model
     model = convnext("base", pretrained=load_weights)
     logits = model(img)
 
@@ -84,11 +93,11 @@ def test_convnext_base_img_classification(device, f, fw, batch_shape, load_weigh
     # Value test
     if load_weights:
         np_out = ivy.to_numpy(logits[0])
-        true_indices = np.array([111, 1, 644, 0, 318])
+        true_indices = np.array([282, 281, 285, 292, 743])
         calc_indices = np.argsort(np_out)[-5:][::-1]
         assert np.array_equal(true_indices, calc_indices)
 
-        true_logits = np.array([2.9980, 2.4199, 2.2514, 2.1669, 2.1209])
+        true_logits = np.array([9.925498, 6.706859, 4.071909, 3.399646, 2.406484])
         calc_logits = np.take(np_out, calc_indices)
         assert np.allclose(true_logits, calc_logits, rtol=1e-3)
 
@@ -98,11 +107,15 @@ def test_convnext_base_img_classification(device, f, fw, batch_shape, load_weigh
 def test_convnext_large_img_classification(device, f, fw, batch_shape, load_weights):
     """Test ConvNeXt large image classification."""
     num_classes = 1000
-    # load image
     this_dir = os.path.dirname(os.path.realpath(__file__))
-    img = ivy.asarray(
-        np.load(os.path.join(this_dir, "image_convnext.npy")),
+    
+    # Load image
+    img = helpers.load_and_preprocess_img(
+        os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
     )
+    img = ivy.permute_dims(img, (0,3,1,2))
+    
+    # Create model
     model = convnext("large", pretrained=load_weights)
 
     logits = model(img)
@@ -113,10 +126,10 @@ def test_convnext_large_img_classification(device, f, fw, batch_shape, load_weig
     # Value test
     if load_weights:
         np_out = ivy.to_numpy(logits[0])
-        true_indices = np.array([0, 111, 78, 623, 940])
+        true_indices = np.array([282, 281, 285, 292, 287])
         calc_indices = np.argsort(np_out)[-5:][::-1]
         assert np.array_equal(true_indices, calc_indices)
 
-        true_logits = np.array([4.2831, 3.3038, 2.4085, 2.3058, 2.1760])
+        true_logits = np.array([9.526691 , 6.26409  , 4.3038955, 3.3073874, 1.7843213])
         calc_logits = np.take(np_out, calc_indices)
         assert np.allclose(true_logits, calc_logits, rtol=1e-3)
