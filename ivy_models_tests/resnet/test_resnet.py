9df4334669bafd9e568afe6a5f5b6ae3def75768
JG <67888047+juliagsy@users.noreply.github.com>
2023-06-26 10:52:57 +0800

    add image helpers and fix resnet18 weights issue (#25)

--- ivy_models_tests/resnet/test_resnet.py
+++ ivy_models_tests/resnet/test_resnet.py
@@ -8,6 +8,7 @@ import jax
 jax.config.update("jax_enable_x64", True)
 
 from ivy_models.resnet import resnet_18
+from ivy_models_tests import helpers
 
 
 @pytest.mark.parametrize("batch_shape", [[1]])
@@ -15,38 +16,31 @@ from ivy_models.resnet import resnet_18
 def test_resnet_18_img_classification(device, f, fw, batch_shape, load_weights):
     """Test ResNet-18 image classification."""
     num_classes = 1000
-    device = "cpu"
     this_dir = os.path.dirname(os.path.realpath(__file__))
 
     # Load image
-    img = ivy.asarray(
-        np.load(os.path.join(this_dir, "img_resnet.npy"))[None],
-        dtype="float32",
-        device=device,
+    img = helpers.load_and_preprocess_img(
+        os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
     )
 
     # Create model
     model = resnet_18(pretrained=load_weights)
 
     # Perform inference
-    output = model(img[0])
-
-    if load_weights:
-        if isinstance(output, ivy.Container):
-            output = output["w"]
+    output = model(img)
 
     # Cardinality test
     assert output.shape == tuple([ivy.to_scalar(batch_shape), num_classes])
 
     # Value test
     if load_weights:
-        np_out = ivy.to_numpy(output[0])
-        true_indices = np.array([287, 285, 281, 282])
-        calc_indices = np.argsort(np_out)[-4:]
+        output = output[0]
+        true_indices = ivy.array([282, 285, 281])
+        calc_indices = ivy.argsort(output, descending=True)[:3]
 
         assert np.array_equal(true_indices, calc_indices)
 
-        true_logits = np.array([10.133721, 11.933505, 12.343024, 13.348762])
-        calc_logits = np.take(np_out, calc_indices)
+        true_logits = ivy.array([13.348762, 12.343024, 11.933505])
+        calc_logits = ivy.take_along_axis(output, calc_indices, 0)
 
         assert np.allclose(true_logits, calc_logits, rtol=0.5)
