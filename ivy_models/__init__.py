f385f9a68c57335c9ac39ef3d361a2edc49fda7c
Tom Edwards <49121269+tomatillos@users.noreply.github.com>
2023-06-27 10:04:55 +0100

    AlexNet implementation (#22)
    * add alexnet + tests
    
    * lint
    
    * lint
    
    * lint
    
    * Using new helper functions to load weights
    
    * lint
    
    * using new image loaders with alexnet tests
    
    * lint
    
    * lint
    
    * small refactor
    
    ---------
    
    Co-authored-by: JG <67888047+juliagsy@users.noreply.github.com>

--- ivy_models/__init__.py
+++ ivy_models/__init__.py
@@ -2,9 +2,11 @@ from . import transformers
 from . import resnet
 from . import vgg
 from . import convnext
+from . import alexnet
 from . import helpers
 from .transformers import perceiver_io
 from .transformers.perceiver_io import *
 from .resnet import *
 from .vgg import *
 from .convnext import *
+from .alexnet import *
