b01a1d90ae7d93be1ebb4ac62a6c138aa21a1a46
JG <67888047+juliagsy@users.noreply.github.com>
2023-06-28 10:25:30 +0800

    update function wrapper name

--- ivy_models/unet/unet.py
+++ ivy_models/unet/unet.py
@@ -1,4 +1,4 @@
-from .unet_layers import *
+from .unet_layers import UNetDoubleConv, UNetDown, UNetOutConv, UNetUp
 from ivy_models.helpers import load_torch_weights
 import ivy
 import builtins
@@ -10,17 +10,17 @@ class UNET(ivy.Module):
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = (UNetDoubleConv(n_channels, 64))
-        self.down1 = (UNetDown(64, 128))
-        self.down2 = (UNetDown(128, 256))
-        self.down3 = (UNetDown(256, 512))
+        self.inc = UNetDoubleConv(n_channels, 64)
+        self.down1 = UNetDown(64, 128)
+        self.down2 = UNetDown(128, 256)
+        self.down3 = UNetDown(256, 512)
         factor = 2 if bilinear else 1
-        self.down4 = (UNetDown(512, 1024 // factor))
-        self.up1 = (UNetUp(1024, 512 // factor, bilinear))
-        self.up2 = (UNetUp(512, 256 // factor, bilinear))
-        self.up3 = (UNetUp(256, 128 // factor, bilinear))
-        self.up4 = (UNetUp(128, 64, bilinear))
-        self.outc = (UNetOutConv(64, n_classes))
+        self.down4 = UNetDown(512, 1024 // factor)
+        self.up1 = UNetUp(1024, 512 // factor, bilinear)
+        self.up2 = UNetUp(512, 256 // factor, bilinear)
+        self.up3 = UNetUp(256, 128 // factor, bilinear)
+        self.up4 = UNetUp(128, 64, bilinear)
+        self.outc = UNetOutConv(64, n_classes)
 
         super(UNET, self).__init__(v=v)
 
@@ -36,7 +36,8 @@ class UNET(ivy.Module):
         x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
-    
+
+
 def _unet_torch_weights_mapping(old_key, new_key):
     new_mapping = new_key
     W_KEY = [
@@ -48,21 +49,23 @@ def _unet_torch_weights_mapping(old_key, new_key):
     if "up/weight" in old_key:
         new_mapping = {"key_chain": new_key, "pattern": "b c h w -> h w b c"}
     elif builtins.any([kc in old_key for kc in W_KEY]):
-        new_mapping = {"key_chain": new_key, "pattern": "b c h w -> w h c b"}
+        new_mapping = {"key_chain": new_key, "pattern": "b c h w -> h w c b"}
     elif "conv/bias" in old_key or "up/bias" in old_key:
         new_mapping = {"key_chain": new_key, "pattern": "h -> 1 1 1 h"}
-    
+
     return new_mapping
 
 
-def UNet(n_channels=3, n_classes=1, v=None, pretrained=False):
-    """UNET model"""
+def unet_carvana(n_channels=3, n_classes=2, v=None, pretrained=True):
     if not pretrained:
         return UNET(n_channels=n_channels, n_classes=n_classes, v=v)
 
     reference_model = UNET(n_channels=n_channels, n_classes=n_classes)
-    url = "https://github.com/milesial/Pytorch-UNet/releases/download/v3.0/unet_carvana_scale1.0_epoch2.pth"
+    url = "https://github.com/milesial/Pytorch-UNet/releases/download/v3.0/unet_carvana_scale1.0_epoch2.pth"  # noqa
     w_clean = load_torch_weights(
-        url, reference_model,raw_keys_to_prune=["num_batches_tracked"] , custom_mapping=_unet_torch_weights_mapping
+        url,
+        reference_model,
+        raw_keys_to_prune=["num_batches_tracked"],
+        custom_mapping=_unet_torch_weights_mapping,
     )
     return UNET(n_channels=n_channels, n_classes=n_classes, v=w_clean)
