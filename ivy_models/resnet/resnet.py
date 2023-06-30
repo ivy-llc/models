ae9c331eebb52f2dd8182abaf85165c85cc31357
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2023-06-30 13:37:16 +0000

    add resnet model

--- ivy_models/resnet/resnet.py
+++ ivy_models/resnet/resnet.py
@@ -1,150 +1,133 @@
 # global
+from typing import List, Optional, Type, Union
 import builtins
+
 import ivy
 import ivy_models
-
-
-class ResidualBlock(ivy.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        """
-        Helper module that is used in the ResNet implementation.
-        :param in_channels: number of input channels
-        :param out_channels: number of output channels
-        :param stride: stride used in the convolutions
-        :param downsample: downsample function used in the residual path
-        """
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.downsample = downsample
-        self.stride = stride
-        self.relu = ivy.ReLU()
-        super(ResidualBlock, self).__init__()
-
-    def _build(self, *args, **kwrgs):
-        self.conv1 = ivy.Conv2D(
-            self.in_channels,
-            self.out_channels,
-            [3, 3],
-            self.stride,
-            1,
-            with_bias=False,
-        )
-        self.bn1 = ivy.BatchNorm2D(self.out_channels)
-        self.conv2 = ivy.Conv2D(
-            self.out_channels, self.out_channels, [3, 3], 1, 1, with_bias=False
-        )
-        self.bn2 = ivy.BatchNorm2D(self.out_channels)
-
-    def _forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        if self.downsample:
-            for layer in self.downsample:
-                residual = layer(residual)
-        out = ivy.add(out, residual)
-        out = self.relu(out)
-        return out
-
-
-class ResLayer(ivy.Module):
-    def __init__(
-        self,
-        block,
-        planes,
-        blocks,
-        current_inplanes,
-        stride=1,
-    ):
-        """Helper module that is used in the ResNet implementation."""
-        self.block = block
-        self.planes = planes
-        self.blocks = blocks
-        self.stride = stride
-        self.current_inplanes = current_inplanes
-        super(ResLayer, self).__init__()
-
-    def _build(self, *args, **kwargs):
-        downsample = None
-        layers = []
-        if self.stride != 1 or self.current_inplanes != self.planes:
-            downsample = [
-                ivy.Conv2D(
-                    self.current_inplanes,
-                    self.planes,
-                    [1, 1],
-                    self.stride,
-                    0,
-                    with_bias=False,
-                ),
-                ivy.BatchNorm2D(self.planes),
-            ]
-
-        layers.append(
-            self.block(self.current_inplanes, self.planes, self.stride, downsample)
-        )
-
-        self.current_inplanes = self.planes
-        for _ in range(1, self.blocks):
-            layers.append(self.block(self.current_inplanes, self.planes))
-        self.layers = layers
-
-    def _forward(self, inputs):
-        out = inputs
-        for layer in self.layers:
-            out = layer(out)
-        return out
+from .layers import conv1x1, BasicBlock, Bottleneck
 
 
 class ResNet(ivy.Module):
-    def __init__(self, block, layers, num_classes=1000, v=None):
-        """
-        Resnet implementation.
-        :param block: residual block used in the network
-        :param layers: list containing the number of blocks per layer
-        :param num_classes: number of classes in the dataset
-        """
-        self.num_classes = num_classes
+    """
+    Residual Neural Network (ResNet) architecture.
+
+    Args:
+        block (Type[Union[BasicBlock, Bottleneck]]): The block type used in the ResNet architecture.
+        layers (List[int]): List of integers specifying the number of blocks in each layer.
+        num_classes (int): Number of output classes. Defaults to 1000.
+        base_width (int): The base width of the ResNet. Defaults to 64.
+        replace_stride_with_dilation (Optional[List[bool]]): List indicating whether to replace stride with dilation.
+        v (ivy.Container): Unused parameter. Can be ignored.
+
+    """
+
+    def __init__(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck]],
+        layers: List[int],
+        num_classes: int = 1000,
+        base_width: int = 64,
+        replace_stride_with_dilation: Optional[List[bool]] = None,
+        v: ivy.Container = None,
+    ) -> None:
+        self.inplanes = 64
+        self.dilation = 1
         self.block = block
         self.layers = layers
-        self.inplanes = 64
-        self.conv1 = ivy.Conv2D(3, 64, [7, 7], 2, 3, with_bias=False)
-        self.bn1 = ivy.BatchNorm2D(64)
+        self.num_classes = num_classes
+        # if replace_stride_with_dilation is None:
+        if replace_stride_with_dilation is None:
+            replace_stride_with_dilation = [False, False, False]
+        self.replace_stride_with_dilation = replace_stride_with_dilation
+
+        self.base_width = base_width
+        super(ResNet, self).__init__(v=v)
+
+    def _build(self, *args, **kwargs):
+        self.conv1 = ivy.Conv2D(3, self.inplanes, [7, 7], 2, 3, with_bias=False)
+        self.bn1 = ivy.BatchNorm2D(self.inplanes)
         self.relu = ivy.ReLU()
         self.maxpool = ivy.MaxPool2D(3, 2, 1)
+        self.layer1 = self._make_layer(self.block, 64, self.layers[0])
+        self.layer2 = self._make_layer(
+            self.block,
+            128,
+            self.layers[1],
+            stride=2,
+            dilate=self.replace_stride_with_dilation[0],
+        )
+        self.layer3 = self._make_layer(
+            self.block,
+            256,
+            self.layers[2],
+            stride=2,
+            dilate=self.replace_stride_with_dilation[1],
+        )
+        self.layer4 = self._make_layer(
+            self.block,
+            512,
+            self.layers[3],
+            stride=2,
+            dilate=self.replace_stride_with_dilation[2],
+        )
+        self.avgpool = ivy.AdaptiveAvgPool2d((1, 1))
+        self.fc = ivy.Linear(512 * self.block.expansion, self.num_classes)
 
-        self.layer1 = ResLayer(
-            self.block, 64, self.layers[0], current_inplanes=64, stride=1
-        )
-        self.layer2 = ResLayer(
-            self.block, 128, self.layers[1], current_inplanes=64, stride=2
-        )
-        self.layer3 = ResLayer(
-            self.block, 256, self.layers[2], current_inplanes=128, stride=2
-        )
-        self.layer4 = ResLayer(
-            self.block, 512, self.layers[3], current_inplanes=256, stride=2
-        )
+    def _make_layer(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck]],
+        planes: int,
+        blocks: int,
+        stride: int = 1,
+        dilate: bool = False,
+    ) -> ivy.Sequential:
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = ivy.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                ivy.BatchNorm2D(planes * block.expansion),
+            )
 
-        self.avgpool = ivy.AvgPool2D(7, 1, "VALID")
-        self.fc = ivy.Linear(512, self.num_classes)
-        super(ResNet, self).__init__()
-        if v is not None:
-            self.v = v
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+                self.base_width,
+                previous_dilation,
+            )
+        )
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                )
+            )
+
+        return ivy.Sequential(*layers)
 
     def _forward(self, x):
+        dtype = x.dtype
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
+        x = ivy.asarray(x, dtype=dtype)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
+        x = ivy.permute_dims(x, (0, 3, 1, 2))
         x = self.avgpool(x)
         x = x.reshape((x.shape[0], -1))
         x = self.fc(x)
@@ -152,7 +135,7 @@ class ResNet(ivy.Module):
 
 
 def _resnet_torch_weights_mapping(old_key, new_key):
-    W_KEY = ["conv1/weight", "conv2/weight", "downsample/0/weight"]
+    W_KEY = ["conv1/weight", "conv2/weight", "conv3/weight", "downsample/0/weight"]
     new_mapping = new_key
     if builtins.any([kc in old_key for kc in W_KEY]):
         new_mapping = {"key_chain": new_key, "pattern": "b c h w -> h w c b"}
@@ -162,9 +145,9 @@ def _resnet_torch_weights_mapping(old_key, new_key):
 def resnet_18(pretrained=True):
     """ResNet-18 model"""
     if not pretrained:
-        return ResNet(ResidualBlock, [2, 2, 2, 2])
+        return ResNet(BasicBlock, [2, 2, 2, 2])
 
-    reference_model = ResNet(ResidualBlock, [2, 2, 2, 2])
+    reference_model = ResNet(BasicBlock, [2, 2, 2, 2])
     url = "https://download.pytorch.org/models/resnet18-f37072fd.pth"
     w_clean = ivy_models.helpers.load_torch_weights(
         url,
@@ -172,19 +155,68 @@ def resnet_18(pretrained=True):
         raw_keys_to_prune=["num_batches_tracked"],
         custom_mapping=_resnet_torch_weights_mapping,
     )
-    return ResNet(ResidualBlock, [2, 2, 2, 2], v=w_clean)
+    return ResNet(BasicBlock, [2, 2, 2, 2], v=w_clean)
 
 
-def resnet_34(v=None):
+def resnet_34(pretrained=True):
     """ResNet-34 model"""
-    return ResNet(ResidualBlock, [3, 4, 6, 3], v=v)
+    if not pretrained:
+        return ResNet(BasicBlock, [3, 4, 6, 3])
+
+    reference_model = ResNet(BasicBlock, [3, 4, 6, 3])
+    url = "https://download.pytorch.org/models/resnet34-333f7ec4.pth"
+    w_clean = ivy_models.helpers.load_torch_weights(
+        url,
+        reference_model,
+        raw_keys_to_prune=["num_batches_tracked"],
+        custom_mapping=_resnet_torch_weights_mapping,
+    )
+    return ResNet(BasicBlock, [3, 4, 6, 3], v=w_clean)
 
 
-def resnet_101(v=None):
+def resnet_50(pretrained=True):
+    """ResNet-50 model"""
+    if not pretrained:
+        return ResNet(Bottleneck, [3, 4, 6, 3])
+
+    reference_model = ResNet(Bottleneck, [3, 4, 6, 3])
+    url = "https://download.pytorch.org/models/resnet50-11ad3fa6.pth"
+    w_clean = ivy_models.helpers.load_torch_weights(
+        url,
+        reference_model,
+        raw_keys_to_prune=["num_batches_tracked"],
+        custom_mapping=_resnet_torch_weights_mapping,
+    )
+    return ResNet(Bottleneck, [3, 4, 6, 3], v=w_clean)
+
+
+def resnet_101(pretrained=True):
     """ResNet-101 model"""
-    return ResNet(ResidualBlock, [3, 4, 23, 3], v=v)
+    if not pretrained:
+        return ResNet(Bottleneck, [3, 4, 23, 3])
+
+    reference_model = ResNet(Bottleneck, [3, 4, 23, 3])
+    url = "https://download.pytorch.org/models/resnet101-cd907fc2.pth"
+    w_clean = ivy_models.helpers.load_torch_weights(
+        url,
+        reference_model,
+        raw_keys_to_prune=["num_batches_tracked"],
+        custom_mapping=_resnet_torch_weights_mapping,
+    )
+    return ResNet(Bottleneck, [3, 4, 23, 3], v=w_clean)
 
 
-def resnet_152(v=None):
+def resnet_152(pretrained=True):
     """ResNet-152 model"""
-    return ResNet(ResidualBlock, [3, 8, 36, 3], v=v)
+    if not pretrained:
+        return ResNet(Bottleneck, [3, 8, 36, 3])
+
+    reference_model = ResNet(Bottleneck, [3, 8, 36, 3])
+    url = "https://download.pytorch.org/models/resnet152-f82ba261.pth"
+    w_clean = ivy_models.helpers.load_torch_weights(
+        url,
+        reference_model,
+        raw_keys_to_prune=["num_batches_tracked"],
+        custom_mapping=_resnet_torch_weights_mapping,
+    )
+    return ResNet(Bottleneck, [3, 8, 36, 3], v=w_clean)
--- ivy_models_tests/resnet/test_resnet.py
+++ ivy_models_tests/resnet/test_resnet.py
@@ -6,9 +6,20 @@ import jax
 
 # Enable x64 support in JAX
 jax.config.update("jax_enable_x64", True)
-
-from ivy_models.resnet import resnet_18
 from ivy_models_tests import helpers
+from ivy_models.resnet import resnet_18, resnet_34, resnet_50, resnet_152, resnet_101
+
+
+def normalize(img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    """prepare image to be the model's input"""
+    img -= np.array(mean)
+    img /= np.array(std)
+    return img
+
+
+def np_softmax(inputs):
+    """apply the softmax on the output"""
+    return np.exp(inputs) / np.sum(np.exp(inputs))
 
 
 @pytest.mark.parametrize("batch_shape", [[1]])
@@ -19,8 +30,10 @@ def test_resnet_18_img_classification(device, f, fw, batch_shape, load_weights):
     this_dir = os.path.dirname(os.path.realpath(__file__))
 
     # Load image
-    img = helpers.load_and_preprocess_img(
-        os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
+    img = ivy.asarray(
+        helpers.load_and_preprocess_img(
+            os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
+        ),
     )
 
     # Create model
@@ -35,12 +48,170 @@ def test_resnet_18_img_classification(device, f, fw, batch_shape, load_weights):
     # Value test
     if load_weights:
         output = output[0]
-        true_indices = ivy.array([282, 285, 281])
+        true_indices = ivy.array([282, 281, 285])
         calc_indices = ivy.argsort(output, descending=True)[:3]
 
         assert np.array_equal(true_indices, calc_indices)
 
-        true_logits = ivy.array([13.348762, 12.343024, 11.933505])
-        calc_logits = ivy.take_along_axis(output, calc_indices, 0)
+        true_logits = np.array([0.7069, 0.2663, 0.0231])
+        calc_logits = np.take(
+            np_softmax(ivy.to_numpy(output)), ivy.to_numpy(calc_indices)
+        )
 
-        assert np.allclose(true_logits, calc_logits, rtol=0.5)
+        assert np.allclose(true_logits, calc_logits, rtol=0.005)
+
+
+@pytest.mark.parametrize("batch_shape", [[1]])
+@pytest.mark.parametrize("load_weights", [False, True])
+def test_resnet_34_img_classification(device, f, fw, batch_shape, load_weights):
+    """Test ResNet-34 image classification."""
+    num_classes = 1000
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Load image
+    img = ivy.asarray(
+        helpers.load_and_preprocess_img(
+            os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
+        )
+    )
+
+    # Create model
+    model = resnet_34(pretrained=load_weights)
+
+    # Perform inference
+    output = model(img)
+
+    # Cardinality test
+    assert output.shape == tuple([ivy.to_scalar(batch_shape), num_classes])
+
+    # Value test
+    if load_weights:
+        output = output[0]
+        true_indices = ivy.array([282, 281, 285])
+        calc_indices = ivy.argsort(output, descending=True)[:3]
+
+        assert np.array_equal(true_indices, calc_indices)
+
+        true_logits = np.array([0.8507, 0.1351, 0.0069])
+        calc_logits = np.take(
+            np_softmax(ivy.to_numpy(output)), ivy.to_numpy(calc_indices)
+        )
+
+        assert np.allclose(true_logits, calc_logits, rtol=0.005)
+
+
+@pytest.mark.parametrize("batch_shape", [[1]])
+@pytest.mark.parametrize("load_weights", [False, True])
+def test_resnet_50_img_classification(device, f, fw, batch_shape, load_weights):
+    """Test ResNet-50 image classification."""
+    num_classes = 1000
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Load image
+    img = ivy.asarray(
+        helpers.load_and_preprocess_img(
+            os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
+        )
+    )
+
+    # Create model
+    model = resnet_50(pretrained=load_weights)
+
+    # Perform inference
+    output = model(img)
+
+    # Cardinality test
+    assert output.shape == tuple([ivy.to_scalar(batch_shape), num_classes])
+
+    # Value test
+    if load_weights:
+        output = output[0]
+        true_indices = ivy.array([282, 281, 285])
+        calc_indices = ivy.argsort(output, descending=True)[:3]
+
+        assert np.array_equal(true_indices, calc_indices)
+
+        true_logits = np.array([0.3429, 0.0408, 0.0121])
+        calc_logits = np.take(
+            np_softmax(ivy.to_numpy(output)), ivy.to_numpy(calc_indices)
+        )
+
+        assert np.allclose(true_logits, calc_logits, rtol=0.005)
+
+
+@pytest.mark.parametrize("batch_shape", [[1]])
+@pytest.mark.parametrize("load_weights", [False, True])
+def test_resnet_101_img_classification(device, f, fw, batch_shape, load_weights):
+    """Test ResNet-101 image classification."""
+    num_classes = 1000
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Load image
+    img = ivy.asarray(
+        helpers.load_and_preprocess_img(
+            os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
+        )
+    )
+
+    # Create model
+    model = resnet_101(pretrained=load_weights)
+
+    # Perform inference
+    output = model(img)
+
+    # Cardinality test
+    assert output.shape == tuple([ivy.to_scalar(batch_shape), num_classes])
+
+    # Value test
+    if load_weights:
+        output = output[0]
+        true_indices = ivy.array([282, 281, 285])
+        calc_indices = ivy.argsort(output, descending=True)[:3]
+
+        assert np.array_equal(true_indices, calc_indices)
+
+        true_logits = np.array([0.7834, 0.0229, 0.0112])
+        calc_logits = np.take(
+            np_softmax(ivy.to_numpy(output)), ivy.to_numpy(calc_indices)
+        )
+
+        assert np.allclose(true_logits, calc_logits, rtol=0.005)
+
+
+@pytest.mark.parametrize("batch_shape", [[1]])
+@pytest.mark.parametrize("load_weights", [False, True])
+def test_resnet_152_img_classification(device, f, fw, batch_shape, load_weights):
+    """Test ResNet-152 image classification."""
+    num_classes = 1000
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Load image
+    img = ivy.asarray(
+        helpers.load_and_preprocess_img(
+            os.path.join(this_dir, "..", "..", "images", "cat.jpg"), 256, 224
+        )
+    )
+
+    # Create model
+    model = resnet_152(pretrained=load_weights)
+
+    # Perform inference
+    output = model(img)
+
+    # Cardinality test
+    assert output.shape == tuple([ivy.to_scalar(batch_shape), num_classes])
+
+    # Value test
+    if load_weights:
+        output = output[0]
+        true_indices = ivy.array([282, 281, 285])
+        calc_indices = ivy.argsort(output, descending=True)[:3]
+
+        assert np.array_equal(true_indices, calc_indices)
+
+        true_logits = np.array([0.8051, 0.0473, 0.0094])
+        calc_logits = np.take(
+            np_softmax(ivy.to_numpy(output)), ivy.to_numpy(calc_indices)
+        )
+
+        assert np.allclose(true_logits, calc_logits, rtol=0.005)
