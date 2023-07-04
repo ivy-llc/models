8366c3f75dbd1cebb99ca526ed398214a6992370
Muhammad Abdullah <abdulah4591@gmail.com>
2023-07-04 09:46:30 +0500

    adding squeezenet in ivy

--- ivy_models/squeezenet/squeezenet.py
+++ ivy_models/squeezenet/squeezenet.py
@@ -1,8 +1,7 @@
 from ivy_models.helpers import load_torch_weights
 import ivy
-import builtins
 
-class Fire(ivy.Module):
+class SqueezeNetFire(ivy.Module):
     def __init__(self, inplanes: int, squeeze_planes: int, expand1x1_planes: int, expand3x3_planes: int) -> None:
         self.inplanes = inplanes
         self.squeeze = ivy.Conv2D(inplanes, squeeze_planes, [1, 1], 1, 0, data_format="NCHW")
@@ -28,32 +27,32 @@ class SqueezeNet(ivy.Module):
                 ivy.Conv2D(3, 96, [7, 7], 2, 0, data_format="NCHW"),
                 ivy.ReLU(),
                 ivy.MaxPool2D(3, 2, 0, data_format="NCHW"),
-                Fire(96, 16, 64, 64),
-                Fire(128, 16, 64, 64),
-                Fire(128, 32, 128, 128),
+                SqueezeNetFire(96, 16, 64, 64),
+                SqueezeNetFire(128, 16, 64, 64),
+                SqueezeNetFire(128, 32, 128, 128),
                 ivy.MaxPool2D(3, 2, 0, data_format="NCHW"),
-                Fire(256, 32, 128, 128),
-                Fire(256, 48, 192, 192),
-                Fire(384, 48, 192, 192),
-                Fire(384, 64, 256, 256),
+                SqueezeNetFire(256, 32, 128, 128),
+                SqueezeNetFire(256, 48, 192, 192),
+                SqueezeNetFire(384, 48, 192, 192),
+                SqueezeNetFire(384, 64, 256, 256),
                 ivy.MaxPool2D(3, 2, 0, data_format="NCHW"),
-                Fire(512, 64, 256, 256),
+                SqueezeNetFire(512, 64, 256, 256),
             )
         elif version == "1_1":
             self.features = ivy.Sequential(
                 ivy.Conv2D(3, 64, [3, 3], 2, 0, data_format="NCHW"),
                 ivy.ReLU(),
                 ivy.MaxPool2D(3, 2, 0, data_format="NCHW"),
-                Fire(64, 16, 64, 64),
-                Fire(128, 16, 64, 64),
+                SqueezeNetFire(64, 16, 64, 64),
+                SqueezeNetFire(128, 16, 64, 64),
                 ivy.MaxPool2D(3, 2, 0, data_format="NCHW"),
-                Fire(128, 32, 128, 128),
-                Fire(256, 32, 128, 128),
+                SqueezeNetFire(128, 32, 128, 128),
+                SqueezeNetFire(256, 32, 128, 128),
                 ivy.MaxPool2D(3, 2, 0, data_format="NCHW"),
-                Fire(256, 48, 192, 192),
-                Fire(384, 48, 192, 192),
-                Fire(384, 64, 256, 256),
-                Fire(512, 64, 256, 256),
+                SqueezeNetFire(256, 48, 192, 192),
+                SqueezeNetFire(384, 48, 192, 192),
+                SqueezeNetFire(384, 64, 256, 256),
+                SqueezeNetFire(512, 64, 256, 256),
             )
         else:
             raise ValueError(f"Unsupported SqueezeNet version {version}: 1_0 or 1_1 expected")
@@ -74,14 +73,14 @@ class SqueezeNet(ivy.Module):
 def _squeezenet_torch_weights_mapping(old_key, new_key):
     new_mapping = new_key
     if "weight" in old_key:
-        new_mapping = {"key_chain": new_key, "pattern": "b c h w -> w h c b"}
+        new_mapping = {"key_chain": new_key, "pattern": "b c h w -> h w c b"}
     elif "bias" in old_key:
         new_mapping = {"key_chain": new_key, "pattern": "h -> 1 h 1 1"}
 
     return new_mapping
 
 
-def squeezeNet(version: str = "1_0", num_classes: int = 1000, dropout: float = 0.5, v=None, pretrained=True):
+def squeezenet(version: str = "1_0", num_classes: int = 1000, dropout: float = 0.5, v=None, pretrained=True):
     if not pretrained:
         return SqueezeNet(version, num_classes, dropout, v=v)
 
