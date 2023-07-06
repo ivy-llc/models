beba0b963a056afd5f23575641c95a57e52bad87
Muhammad Abdullah <abdulah4591@gmail.com>
2023-07-06 15:52:36 +0500

    Adding DenseNet model in Ivy

--- ivy_models/densenet/densenet.py
+++ ivy_models/densenet/densenet.py
@@ -1,23 +1,13 @@
-import re
 from collections import OrderedDict
-from functools import partial
-from typing import Any, List, Optional, Tuple
+from typing import Tuple
 
+from ivy_models.helpers import load_torch_weights
 import ivy
 
-__all__ = [
-    "DenseNet",
-    "densenet121",
-    "densenet161",
-    "densenet169",
-    "densenet201",
-]
-
-# should add _build 
 
 class _DenseLayer(ivy.Module):
     def __init__(
-        self, num_input_features: int, growth_rate: int, bn_size: int, drop_rate: float, memory_efficient: bool = False
+        self, num_input_features: int, growth_rate: int, bn_size: int, drop_rate: float
     ) -> None:
         self.norm1 = ivy.BatchNorm2D(num_input_features)
         self.relu1 = ivy.ReLU()
@@ -28,41 +18,25 @@ class _DenseLayer(ivy.Module):
         self.conv2 = ivy.Conv2D(bn_size * growth_rate, growth_rate, [3, 3], 1, 1, with_bias=False, data_format="NCHW")
 
         self.drop_rate = float(drop_rate)
-        self.memory_efficient = memory_efficient
         super().__init__()
 
     def bn_function(self, inputs):
         concated_features = ivy.concat(inputs, 1)
-        bottleneck_output = self.conv1(self.relu1(self.norm1(concated_features))) 
+        bottleneck_output = self.conv1(self.relu1(self.norm1(concated_features)))  # noqa: T484
         return bottleneck_output
 
-    # def call_checkpoint_bottleneck(self, input):
-    #     def closure(*inputs):
-    #         return self.bn_function(inputs)
 
-    #     return cp.checkpoint(closure, *input)
-    
-
-    def any_requires_grad(self, input):
-        for tensor in input:
-            if tensor.requires_grad:
-                return True
-        return False
-
-    def _forward(self, input):  # noqa: F811
-        if isinstance(input, ivy.array):
+    # allowing it to take either a List[Tensor] or single Tensor
+    def _forward(self, input):  
+        if isinstance(input, ivy.Array):
             prev_features = [input]
         else:
             prev_features = input
 
-        # if self.memory_efficient and self.any_requires_grad(prev_features):
-        #     bottleneck_output = self.call_checkpoint_bottleneck(prev_features)
-        # else:
         bottleneck_output = self.bn_function(prev_features)
 
         new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
         if self.drop_rate > 0:
-            # ----------- stopped here -------------
             new_features = ivy.dropout(new_features, prob=self.drop_rate, training=self.training)
         return new_features
 
@@ -76,34 +50,33 @@ class _DenseBlock(ivy.Module):
         bn_size: int,
         growth_rate: int,
         drop_rate: float,
-        memory_efficient: bool = False,
     ) -> None:
+        layers = []
         for i in range(num_layers):
             layer = _DenseLayer(
                 num_input_features + i * growth_rate,
                 growth_rate=growth_rate,
                 bn_size=bn_size,
                 drop_rate=drop_rate,
-                memory_efficient=memory_efficient,
             )
-            self.add_module("denselayer%d" % (i + 1), layer)
+            layers.append(("denselayer%d" % (i + 1), layer))
+        self.layer = ivy.Sequential(*layers)
+
         super().__init__()
+
 
     def _forward(self, init_features):
         features = [init_features]
-        for name, layer in self.named_children():
-            new_features = layer(features)
-            features.append(new_features)
-        return ivy.concat(features, 1)
-
+        return self.layer(features)
+    
 
 class _Transition(ivy.Sequential):
     def __init__(self, num_input_features: int, num_output_features: int) -> None:
+        super().__init__()
         self.norm = ivy.BatchNorm2D(num_input_features)
         self.relu = ivy.ReLU()
         self.conv = ivy.Conv2D(num_input_features, num_output_features, [1, 1], 1, 0, with_bias=False, data_format="NCHW")
-        self.pool = ivy.AvgPool2D(kernel_size=2, stride=2, data_format="NCHW")
-        super().__init__()
+        self.pool = ivy.AvgPool2D(2, 2, 0)
 
 
 class DenseNet(ivy.Module):
@@ -118,7 +91,6 @@ class DenseNet(ivy.Module):
           (i.e. bn_size * k features in the bottleneck layer)
         drop_rate (float) - dropout rate after each dense layer
         num_classes (int) - number of classification classes
-        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
           but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_.
     """
 
@@ -130,21 +102,19 @@ class DenseNet(ivy.Module):
         bn_size: int = 4,
         drop_rate: float = 0,
         num_classes: int = 1000,
-        memory_efficient: bool = False,
+        v=None
     ) -> None:
 
 
         # First convolution
-        self.features = ivy.Sequential(
-            OrderedDict(
+        layers = OrderedDict(
                 [
-                    ("conv0", ivy.Conv2D(3, num_init_features, [7, 7], 2, 3, with_bias=False, data_format="NCHW" )),
+                    ("conv0", ivy.Conv2D(3, num_init_features, [7, 7], 2, 3, with_bias=False, data_format="NCHW")),
                     ("norm0", ivy.BatchNorm2D(num_init_features)),
                     ("relu0", ivy.ReLU()),
                     ("pool0", ivy.MaxPool2D(3, 2, 1, data_format="NCHW")),
                 ]
             )
-        )
 
         # Each denseblock
         num_features = num_init_features
@@ -155,271 +125,115 @@ class DenseNet(ivy.Module):
                 bn_size=bn_size,
                 growth_rate=growth_rate,
                 drop_rate=drop_rate,
-                memory_efficient=memory_efficient,
             )
-            self.features.add_module("denseblock%d" % (i + 1), block)
+            layers["denseblock%d" % (i + 1)] = block
             num_features = num_features + num_layers * growth_rate
             if i != len(block_config) - 1:
                 trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
-                self.features.add_module("transition%d" % (i + 1), trans)
+                layers["transition%d" % (i + 1)] = trans
                 num_features = num_features // 2
 
         # Final batch norm
-        self.features.add_module("norm5", ivy.BatchNorm2D(num_features))
+        layers["norm5"] = ivy.BatchNorm2D(num_features)
+
+        self.features = ivy.Sequential(layers)
 
         # Linear layer
         self.classifier = ivy.Linear(num_features, num_classes)
 
-        super().__init__()
-        # Official init from torch repo.
-        # for m in self.modules():
-        #     if isinstance(m, nn.Conv2d):
-        #         nn.init.kaiming_normal_(m.weight)
-        #     elif isinstance(m, nn.BatchNorm2d):
-        #         nn.init.constant_(m.weight, 1)
-        #         nn.init.constant_(m.bias, 0)
-        #     elif isinstance(m, nn.Linear):
-        #         nn.init.constant_(m.bias, 0)
+        super().__init__(v=v)
 
     def _forward(self, x):
         features = self.features(x)
-        out = ivy.relu(features, inplace=True)
+        out = ivy.relu(features, )
         out = ivy.adaptive_avg_pool2d(out, (1, 1))
         out = ivy.flatten(out, 1)
         out = self.classifier(out)
         return out
 
+def _densenet_torch_weights_mapping(old_key, new_key):
+    new_mapping = new_key
+    if "features" in old_key:
+        if "conv0/weight" in old_key or "conv/1/weight" in old_key or "conv/2/weight" in old_key:
+            new_mapping = {"key_chain": new_key, "pattern": "b c h w-> h w c b"}
+    return new_mapping
+
+def densenet(
+    growth_rate: int,
+    block_config: Tuple[int, int, int, int],
+    num_init_features: int,
+    v=None,
+):
+    model = DenseNet(growth_rate, block_config, num_init_features, v=v)
+    return model
+
+
+def densenet121(
+    v=None,
+    pretrained=True
+):
+    ref_model = densenet(32, (6, 12, 24, 16), 64, v=v)
+    if pretrained:
+        url = "https://download.pytorch.org/models/densenet121-a639ec97.pth"
+        w_clean = load_torch_weights(
+            url,
+            ref_model,
+            raw_keys_to_prune=["num_batches_tracked"],
+            custom_mapping=_densenet_torch_weights_mapping,
+        )
+    return densenet(32, (6, 12, 24, 16), 64, v=w_clean)
+
+
+def densenet161(
+        v=None,
+        pretrained=True
+):
+    ref_model = densenet(48, (6, 12, 36, 24), 96, v=v)
+    if pretrained:
+        url = "https://download.pytorch.org/models/densenet161-17b70270.pth"
+        w_clean = load_torch_weights(
+            url,
+            ref_model,
+            raw_keys_to_prune=["num_batches_tracked"],
+            custom_mapping=_densenet_torch_weights_mapping,
+        )
+    return densenet(48, (6, 12, 36, 24), 96, v=w_clean)
+
+ivy.add()
+def densenet169(
+        v=None,
+        pretrained=True      
+):
+    ref_model = densenet(32, (6, 12, 32, 32), 64, v=v)
+    if pretrained:
+        url = "https://download.pytorch.org/models/densenet169-b2777c0a.pth"
+        w_clean = load_torch_weights(
+            url,
+            ref_model,
+            raw_keys_to_prune=["num_batches_tracked"],
+            custom_mapping=_densenet_torch_weights_mapping,
+        )
+    return densenet(32, (6, 12, 32, 32), 64, v=w_clean)
+
+
+def densenet201(
+        v=None,
+        pretrained=True
+):
+    ref_model = densenet(32, (6, 12, 48, 32), 64, v=v)
+    if pretrained:
+        url = "https://download.pytorch.org/models/densenet201-c1103571.pth"
+        w_clean = load_torch_weights(
+            url,
+            ref_model,
+            raw_keys_to_prune=["num_batches_tracked"],
+            custom_mapping=_densenet_torch_weights_mapping,
+        )
+    return densenet(32, (6, 12, 48, 32), 64, v=w_clean)
+
+
 
 if __name__ == "__main__":
     ivy.set_torch_backend()
-    model = DenseNet()
+    model = DenseNet(growth_rate=32, block_config=(6, 12, 24, 16), num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000)
     print(model.v)
-
-# def _load_state_dict(model: nn.Module, weights: WeightsEnum, progress: bool) -> None:
-#     # '.'s are no longer allowed in module names, but previous _DenseLayer
-#     # has keys 'norm.1', 'relu.1', 'conv.1', 'norm.2', 'relu.2', 'conv.2'.
-#     # They are also in the checkpoints in model_urls. This pattern is used
-#     # to find such keys.
-#     pattern = re.compile(
-#         r"^(.*denselayer\d+\.(?:norm|relu|conv))\.((?:[12])\.(?:weight|bias|running_mean|running_var))$"
-#     )
-
-#     state_dict = weights.get_state_dict(progress=progress, check_hash=True)
-#     for key in list(state_dict.keys()):
-#         res = pattern.match(key)
-#         if res:
-#             new_key = res.group(1) + res.group(2)
-#             state_dict[new_key] = state_dict[key]
-#             del state_dict[key]
-#     model.load_state_dict(state_dict)
-
-
-# def _densenet(
-#     growth_rate: int,
-#     block_config: Tuple[int, int, int, int],
-#     num_init_features: int,
-#     weights: Optional[WeightsEnum],
-#     progress: bool,
-#     **kwargs: Any,
-# ) -> DenseNet:
-#     if weights is not None:
-#         _ovewrite_named_param(kwargs, "num_classes", len(weights.meta["categories"]))
-
-#     model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
-
-#     if weights is not None:
-#         _load_state_dict(model=model, weights=weights, progress=progress)
-
-#     return model
-
-
-# _COMMON_META = {
-#     "min_size": (29, 29),
-#     "categories": _IMAGENET_CATEGORIES,
-#     "recipe": "https://github.com/pytorch/vision/pull/116",
-#     "_docs": """These weights are ported from LuaTorch.""",
-# }
-
-
-# class DenseNet121_Weights(WeightsEnum):
-#     IMAGENET1K_V1 = Weights(
-#         url="https://download.pytorch.org/models/densenet121-a639ec97.pth",
-#         transforms=partial(ImageClassification, crop_size=224),
-#         meta={
-#             **_COMMON_META,
-#             "num_params": 7978856,
-#             "_metrics": {
-#                 "ImageNet-1K": {
-#                     "acc@1": 74.434,
-#                     "acc@5": 91.972,
-#                 }
-#             },
-#             "_ops": 2.834,
-#             "_file_size": 30.845,
-#         },
-#     )
-#     DEFAULT = IMAGENET1K_V1
-
-
-# class DenseNet161_Weights(WeightsEnum):
-#     IMAGENET1K_V1 = Weights(
-#         url="https://download.pytorch.org/models/densenet161-8d451a50.pth",
-#         transforms=partial(ImageClassification, crop_size=224),
-#         meta={
-#             **_COMMON_META,
-#             "num_params": 28681000,
-#             "_metrics": {
-#                 "ImageNet-1K": {
-#                     "acc@1": 77.138,
-#                     "acc@5": 93.560,
-#                 }
-#             },
-#             "_ops": 7.728,
-#             "_file_size": 110.369,
-#         },
-#     )
-#     DEFAULT = IMAGENET1K_V1
-
-
-# class DenseNet169_Weights(WeightsEnum):
-#     IMAGENET1K_V1 = Weights(
-#         url="https://download.pytorch.org/models/densenet169-b2777c0a.pth",
-#         transforms=partial(ImageClassification, crop_size=224),
-#         meta={
-#             **_COMMON_META,
-#             "num_params": 14149480,
-#             "_metrics": {
-#                 "ImageNet-1K": {
-#                     "acc@1": 75.600,
-#                     "acc@5": 92.806,
-#                 }
-#             },
-#             "_ops": 3.36,
-#             "_file_size": 54.708,
-#         },
-#     )
-#     DEFAULT = IMAGENET1K_V1
-
-
-# class DenseNet201_Weights(WeightsEnum):
-#     IMAGENET1K_V1 = Weights(
-#         url="https://download.pytorch.org/models/densenet201-c1103571.pth",
-#         transforms=partial(ImageClassification, crop_size=224),
-#         meta={
-#             **_COMMON_META,
-#             "num_params": 20013928,
-#             "_metrics": {
-#                 "ImageNet-1K": {
-#                     "acc@1": 76.896,
-#                     "acc@5": 93.370,
-#                 }
-#             },
-#             "_ops": 4.291,
-#             "_file_size": 77.373,
-#         },
-#     )
-#     DEFAULT = IMAGENET1K_V1
-
-
-# @register_model()
-# @handle_legacy_interface(weights=("pretrained", DenseNet121_Weights.IMAGENET1K_V1))
-# def densenet121(*, weights: Optional[DenseNet121_Weights] = None, progress: bool = True, **kwargs: Any) -> DenseNet:
-#     r"""Densenet-121 model from
-#     `Densely Connected Convolutional Networks <https://arxiv.org/abs/1608.06993>`_.
-
-#     Args:
-#         weights (:class:`~torchvision.models.DenseNet121_Weights`, optional): The
-#             pretrained weights to use. See
-#             :class:`~torchvision.models.DenseNet121_Weights` below for
-#             more details, and possible values. By default, no pre-trained
-#             weights are used.
-#         progress (bool, optional): If True, displays a progress bar of the download to stderr. Default is True.
-#         **kwargs: parameters passed to the ``torchvision.models.densenet.DenseNet``
-#             base class. Please refer to the `source code
-#             <https://github.com/pytorch/vision/blob/main/torchvision/models/densenet.py>`_
-#             for more details about this class.
-
-#     .. autoclass:: torchvision.models.DenseNet121_Weights
-#         :members:
-#     """
-#     weights = DenseNet121_Weights.verify(weights)
-
-#     return _densenet(32, (6, 12, 24, 16), 64, weights, progress, **kwargs)
-
-
-# @register_model()
-# @handle_legacy_interface(weights=("pretrained", DenseNet161_Weights.IMAGENET1K_V1))
-# def densenet161(*, weights: Optional[DenseNet161_Weights] = None, progress: bool = True, **kwargs: Any) -> DenseNet:
-#     r"""Densenet-161 model from
-#     `Densely Connected Convolutional Networks <https://arxiv.org/abs/1608.06993>`_.
-
-#     Args:
-#         weights (:class:`~torchvision.models.DenseNet161_Weights`, optional): The
-#             pretrained weights to use. See
-#             :class:`~torchvision.models.DenseNet161_Weights` below for
-#             more details, and possible values. By default, no pre-trained
-#             weights are used.
-#         progress (bool, optional): If True, displays a progress bar of the download to stderr. Default is True.
-#         **kwargs: parameters passed to the ``torchvision.models.densenet.DenseNet``
-#             base class. Please refer to the `source code
-#             <https://github.com/pytorch/vision/blob/main/torchvision/models/densenet.py>`_
-#             for more details about this class.
-
-#     .. autoclass:: torchvision.models.DenseNet161_Weights
-#         :members:
-#     """
-#     weights = DenseNet161_Weights.verify(weights)
-
-#     return _densenet(48, (6, 12, 36, 24), 96, weights, progress, **kwargs)
-
-
-# @register_model()
-# @handle_legacy_interface(weights=("pretrained", DenseNet169_Weights.IMAGENET1K_V1))
-# def densenet169(*, weights: Optional[DenseNet169_Weights] = None, progress: bool = True, **kwargs: Any) -> DenseNet:
-#     r"""Densenet-169 model from
-#     `Densely Connected Convolutional Networks <https://arxiv.org/abs/1608.06993>`_.
-
-#     Args:
-#         weights (:class:`~torchvision.models.DenseNet169_Weights`, optional): The
-#             pretrained weights to use. See
-#             :class:`~torchvision.models.DenseNet169_Weights` below for
-#             more details, and possible values. By default, no pre-trained
-#             weights are used.
-#         progress (bool, optional): If True, displays a progress bar of the download to stderr. Default is True.
-#         **kwargs: parameters passed to the ``torchvision.models.densenet.DenseNet``
-#             base class. Please refer to the `source code
-#             <https://github.com/pytorch/vision/blob/main/torchvision/models/densenet.py>`_
-#             for more details about this class.
-
-#     .. autoclass:: torchvision.models.DenseNet169_Weights
-#         :members:
-#     """
-#     weights = DenseNet169_Weights.verify(weights)
-
-#     return _densenet(32, (6, 12, 32, 32), 64, weights, progress, **kwargs)
-
-
-# @register_model()
-# @handle_legacy_interface(weights=("pretrained", DenseNet201_Weights.IMAGENET1K_V1))
-# def densenet201(*, weights: Optional[DenseNet201_Weights] = None, progress: bool = True, **kwargs: Any) -> DenseNet:
-#     r"""Densenet-201 model from
-#     `Densely Connected Convolutional Networks <https://arxiv.org/abs/1608.06993>`_.
-
-#     Args:
-#         weights (:class:`~torchvision.models.DenseNet201_Weights`, optional): The
-#             pretrained weights to use. See
-#             :class:`~torchvision.models.DenseNet201_Weights` below for
-#             more details, and possible values. By default, no pre-trained
-#             weights are used.
-#         progress (bool, optional): If True, displays a progress bar of the download to stderr. Default is True.
-#         **kwargs: parameters passed to the ``torchvision.models.densenet.DenseNet``
-#             base class. Please refer to the `source code
-#             <https://github.com/pytorch/vision/blob/main/torchvision/models/densenet.py>`_
-#             for more details about this class.
-
-#     .. autoclass:: torchvision.models.DenseNet201_Weights
-#         :members:
-#     """
-#     weights = DenseNet201_Weights.verify(weights)
-
-#     return _densenet(32, (6, 12, 48, 32), 64, weights, progress, **kwargs)
