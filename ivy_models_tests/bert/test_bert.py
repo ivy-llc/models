3b961a04034cf36f493213105d43f5f9a8002217
juliagsy <67888047+juliagsy@users.noreply.github.com>
2023-08-03 11:29:58 +0800

    update path

--- ivy_models_tests/bert/test_bert.py
+++ ivy_models_tests/bert/test_bert.py
@@ -1,3 +1,4 @@
+import os
 import ivy
 import pytest
 import numpy as np
@@ -10,11 +11,16 @@ def test_bert(device, fw, batch_shape, load_weights):
     """Test Bert Base Sequence Classification"""
 
     num_dims = 768
-    inputs = np.load("bert_inputs.npy").tolist()
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
+    img_path = os.path.join(this_dir, "bert_inputs.npy")
+    inputs = np.load(img_path).tolist()
     model = bert_base_uncased(load_weights)
 
     inputs = {k: ivy.asarray(v) for k, v in inputs.items()}
     logits = model(**inputs)["pooler_output"]
-    ref_logits = np.load("bert_pooled_output.npy")
+
+    logits_path = os.path.join(this_dir, "bert_pooled_output.npy")
+    ref_logits = np.load(logits_path)
     assert logits.shape == tuple([ivy.to_scaler(batch_shape), num_dims])
     assert np.allclose(ref_logits, logits, rtol=0.005, atol=0.0005)
