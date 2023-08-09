6052bce4bba094f03f9ad01b84e8fc0fc0e3fad7
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-08-09 10:20:07 +0000

    Updated layers.py

--- ivy_models/regnet/layers.py
+++ ivy_models/regnet/layers.py
@@ -6,7 +6,6 @@ import math
 import collections
 from collections import OrderedDict
 from itertools import repeat
-import warnings
 
 
 def _make_ntuple(x: Any, n: int) -> Tuple[Any, ...]:
@@ -98,8 +97,8 @@ class ConvNormActivation(ivy.Sequential):
         self.out_channels = out_channels
 
         if self.__class__ == ConvNormActivation:
-            warnings.warn(
-                "Don't use ConvNormActivation directly, please use Conv2dNormActivation and Conv3dNormActivation instead."  # noqa: E501
+            ivy.warnings.warn(
+                "Don't use ConvNormActivation directly, please use Conv2dNormActivation instead."
             )
 
 
@@ -108,18 +107,17 @@ class Conv2dNormActivation(ConvNormActivation):
     Configurable block used for Convolution2d-Normalization-Activation blocks.
 
     Args:
-    ----
         in_channels (int): Number of channels in the input image
-        out_channels (int): Number of channels produced by the Convolution-Normalization-Activation block # noqa: E501
+        out_channels (int): Number of channels produced by the Convolution-Normalization-Activation block
         kernel_size: (int, optional): Size of the convolving kernel. Default: 3
         stride (int, optional): Stride of the convolution. Default: 1
-        padding (int, tuple or str, optional): Padding added to all four sides of the input. Default: None, in which case it will be calculated as ``padding = (kernel_size - 1) // 2 * dilation`` # noqa: E501
-        groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1 # noqa: E501
-        norm_layer (Callable[..., torch.nn.Module], optional): Norm layer that will be stacked on top of the convolution layer. If ``None`` this layer won't be used. Default: ``torch.nn.BatchNorm2d`` # noqa: E501
-        activation_layer (Callable[..., torch.nn.Module], optional): Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the conv layer. If ``None`` this layer won't be used. Default: ``torch.nn.ReLU`` # noqa: E501
+        padding (int, tuple or str, optional): Padding added to all four sides of the input. Default: None, in which case it will be calculated as ``padding = (kernel_size - 1) // 2 * dilation``
+        groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
+        norm_layer (Callable[..., ivy.Module], optional): Norm layer that will be stacked on top of the convolution layer. If ``None`` this layer won't be used. Default: ``ivy.BatchNorm2D``
+        activation_layer (Callable[..., ivy.Module], optional): Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the conv layer. If ``None`` this layer won't be used. Default: ``ivy.ReLU``
         dilation (int): Spacing between kernel elements. Default: 1
-        inplace (bool): Parameter for the activation layer, which can optionally do the operation in-place. Default ``True`` # noqa: E501
-        bias (bool, optional): Whether to use bias in the convolution layer. By default, biases are included if ``norm_layer is None``. # noqa: E501
+        inplace (bool): Parameter for the activation layer, which can optionally do the operation in-place. Default ``True``
+        bias (bool, optional): Whether to use bias in the convolution layer. By default, biases are included if ``norm_layer is None``.
 
     """
 
@@ -149,7 +147,7 @@ class Conv2dNormActivation(ConvNormActivation):
             dilation,
             inplace,
             bias,
-            ivy.stateful.Conv2D,
+            ivy.Conv2D,
         )
 
 
